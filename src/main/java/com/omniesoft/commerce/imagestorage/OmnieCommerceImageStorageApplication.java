f34eec2d4ce27cc8300794ed7ef99dbec76b0efc
Alexander Balyshyn <balyszyn@gmail.com>
2018-02-05 15:24:11 +0200

    add super user and changes to permissions verification

--- src/main/java/com/omniesoft/commerce/imagestorage/OmnieCommerceImageStorageApplication.java
+++ src/main/java/com/omniesoft/commerce/imagestorage/OmnieCommerceImageStorageApplication.java
@@ -3,6 +3,8 @@ package com.omniesoft.commerce.imagestorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -10,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableMongoRepositories
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class OmnieCommerceImageStorageApplication {
 
     public static void main(String[] args) {
